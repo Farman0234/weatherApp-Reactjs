fefaba29007a68baac81b7af9d491ec25e64408f
Farman Anees <farmananees831@gmail.com>
2025-07-27 17:50:23 +0500

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "d229727eaf3bf5f4229163fc6ea0e2bb",
   base: "https://api.openweathermap.org/data/2.5/",
 };
